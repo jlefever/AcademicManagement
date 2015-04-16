1fbb7904144a6ffef5ce3c1d5e42748bdcd9b9ae
Juan Salazar <jsalaza1@ycp.edu>
2015-04-16 16:37:26 -0400

    using stripes examples

--- src/main/java/net/sourceforge/stripes/examples/quickstart/CalculatorActionBean.java
+++ src/main/java/net/sourceforge/stripes/examples/quickstart/CalculatorActionBean.java
@@ -1,53 +1,58 @@
 package net.sourceforge.stripes.examples.quickstart;
 
-import net.sourceforge.stripes.action.DefaultHandler;
-import net.sourceforge.stripes.action.Resolution;
-import net.sourceforge.stripes.action.ForwardResolution;
 import net.sourceforge.stripes.action.ActionBean;
 import net.sourceforge.stripes.action.ActionBeanContext;
+import net.sourceforge.stripes.action.DefaultHandler;
+import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.Resolution;
+import net.sourceforge.stripes.validation.SimpleError;
+import net.sourceforge.stripes.validation.Validate;
+import net.sourceforge.stripes.validation.ValidationErrors;
+import net.sourceforge.stripes.validation.ValidationMethod;
 
-/** * A very simple calculator action. * @author Tim Fennell */
+/**
+ * A very simple calculator action.
+ * @author Tim Fennell
+ */
 public class CalculatorActionBean implements ActionBean {
-	private ActionBeanContext context;
-	private double numberOne;
-	private double numberTwo;
-	private double result;
+    private ActionBeanContext context;
+    @Validate(required=true) private double numberOne;
+    @Validate(required=true) private double numberTwo;
+    private double result;
 
-	public ActionBeanContext getContext() {
-		return context;
-	}
+    public ActionBeanContext getContext() { return context; }
+    public void setContext(ActionBeanContext context) { this.context = context; }
 
-	public void setContext(ActionBeanContext context) {
-		this.context = context;
-	}
+    public double getNumberOne() { return numberOne; }
+    public void setNumberOne(double numberOne) { this.numberOne = numberOne; }
 
-	public double getNumberOne() {
-		return numberOne;
-	}
+    public double getNumberTwo() { return numberTwo; }
+    public void setNumberTwo(double numberTwo) { this.numberTwo = numberTwo; }
 
-	public void setNumberOne(double numberOne) {
-		this.numberOne = numberOne;
-	}
+    public double getResult() { return result; }
+    public void setResult(double result) { this.result = result; }
 
-	public double getNumberTwo() {
-		return numberTwo;
-	}
+    /** An event handler method that adds number one to number two. */
+    @DefaultHandler
+    public Resolution addition() {
+        result = numberOne + numberTwo;
+        return new ForwardResolution("/quickstart/index.jsp");
+    }
 
-	public void setNumberTwo(double numberTwo) {
-		this.numberTwo = numberTwo;
-	}
+    /** An event handler method that divides number one by number two. */
+    public Resolution division() {
+        result = numberOne / numberTwo;
+        return new ForwardResolution("/quickstart/index.jsp");
+    }
 
-	public double getResult() {
-		return result;
-	}
-
-	public void setResult(double result) {
-		this.result = result;
-	}
-
-	@DefaultHandler
-	public Resolution addition() {
-		result = getNumberOne() + getNumberTwo();
-		return new ForwardResolution("/quickstart/index.jsp");
-	}
+    /**
+     * An example of a custom validation that checks that division operations
+     * are not dividing by zero.
+     */
+    @ValidationMethod(on="division")
+    public void avoidDivideByZero(ValidationErrors errors) {
+        if (this.numberTwo == 0) {
+            errors.add("numberTwo", new SimpleError("Dividing by zero is not allowed."));
+        }
+    }
 }
